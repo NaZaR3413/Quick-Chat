2b1d3dc7d86f3af953e4467832b1f93cc1652ea1
Nilay Patel <nilayno1@gmail.com>
2024-08-30 19:48:54 -0700

    feat(user management) - setup user-management functionality in our BE application

--- springboot/src/main/java/com/example/quickchat/springboot/config/WebsocketConfig.java
+++ springboot/src/main/java/com/example/quickchat/springboot/config/WebsocketConfig.java
@@ -1,10 +1,17 @@
 package com.example.quickchat.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import com.example.quickchat.springboot.controller.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,6 +27,44 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // path for websocket connections
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS()
+            .setInterceptors(new HttpSessionHandshakeInterceptor() {
+                        @Override
+                        public void afterHandshake(
+                                ServerHttpRequest request,
+                                ServerHttpResponse response,
+                                WebSocketHandler wsHandler,
+                                Exception ex) {
+                        }
+                    });
+    }
+
+    // user connections handler 
+    public class CustomWebSocketHandler extends TextWebSocketHandler 
+    {
+        private ChatController chatController;
+
+        public CustomWebSocketHandler(ChatController _chatController)
+        {
+            chatController = _chatController;
+        }
+
+        public void afterConnectionEstablished(WebSocketSession session) throws Exception
+        {
+            String username = getUsernameFromSession(session); // TO-DO - finish implementation
+            chatController.registerUser(username, session);
+        }
+
+        public void afterConnectionClosed(WebSocketSession session) throws Exception
+        {
+            String username = getUsernameFromSession(session); // TO-DO - finish implementation
+            chatController.unregisterUser(username);
+        }
+
+        // TO-DO implement a way to get a username from the current session 
+        public String getUsernameFromSession(WebSocketSession session)
+        {
+            return "username";
+        }
     }
 }
--- springboot/src/main/java/com/example/quickchat/springboot/controller/ChatController.java
+++ springboot/src/main/java/com/example/quickchat/springboot/controller/ChatController.java
@@ -1,11 +1,14 @@
 package com.example.quickchat.springboot.controller;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.WebSocketSession;
 
 import com.example.quickchat.springboot.model.Message;
 
@@ -14,6 +17,9 @@ public class ChatController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+
+    // In memory storage for user management
+    private ConcurrentHashMap<String, WebSocketSession> connectedUsers = new ConcurrentHashMap<String, WebSocketSession>();
     
     // return public messages to each user 
     @MessageMapping("/message") // /app/message
@@ -27,8 +33,46 @@ public class ChatController {
     @MessageMapping("/private-message")
     public Message recievePrivateMessage(@Payload Message message)
     {
-        // /user/<NAME>/private
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/priavate", message);
+        // verify user's existence and send the message
+        String receiver = message.getReceiverName();
+        if(connectedUsers.containsKey(receiver))
+        {
+            // /user/<NAME>/private
+            simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/priavate", message);
+        }
+        else
+        {
+            System.out.println("User " + receiver + " is not connected");
+        }
+
         return message;
+    }
+
+    // register a new user when they connect
+    public void registerUser(String username, WebSocketSession session)
+    {
+        connectedUsers.put(username, session);
+        simpMessagingTemplate.convertAndSend("/chatroom/public", username + " has connected");
+    }
+
+    // unregister a user when they disconnect 
+    public void unregisterUser(String username)
+    {
+        // verify username's existence and remove it 
+        if(connectedUsers.containsKey(username))
+        {
+            connectedUsers.remove(username);
+            simpMessagingTemplate.convertAndSend("/chatroom/public", username + " has disconnected");
+        }
+        else
+        {
+            System.out.println(username + " does not exist, removal unsuccessful");
+        }
+    }
+
+    // brodcast current userlist to all connected clients
+    private void brodcastUserList()
+    {
+        simpMessagingTemplate.convertAndSend("/chatroom/public", connectedUsers.keySet());
     }
 }
